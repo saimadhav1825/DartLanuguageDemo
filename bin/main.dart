a23e71752995a413381bc7e60c05c5fa34bd3886
saimadhav1825 <saimadhav12342gmail.com>
2022-03-22 09:38:00 +0530

    data types commit

--- bin/main.dart
+++ bin/main.dart
@@ -1,3 +1,7 @@
+
+
+import 'enumdart.dart';
+
 void main() {
   print("Hello world");
   dataTypes();
@@ -28,9 +32,35 @@ void dataTypes() {
   bool bo = true;
   print(bo);
 
+  //List are two types
+  //Fixed Length List -> var list=new List(10);
+  // Growable List   -> var list=new List();
+
+  //Dart Set
+  //var setName = <type>{};
+  // Or
+  // Set<type> setname = {};
+
+  var sets=<String>{"sai","siva","sujith"};
+  sets.forEach((element) {
+    print(element);
+  });
+
+  Set<int> setInt={1,2,3,4};
+  setInt.forEach((element) {print(element);});
+
+
   //Dart MAP
+  //Dart Map can be defined in two methods.
+  //
+  // Using Map Literal-->var map = [1, 2, 3];
+  // Using Map Constructor--->var student = new Map();
   var map = [1, 2, 3];
   print(map);
+
+  var maps=new Map();
+   maps["1"]='sai';
+   maps.forEach((key, value) {print(key+" " +value);});
 
   //Runes
   var heart_symbol = '\u2665';
@@ -44,16 +74,34 @@ void dataTypes() {
   //we cannot use 'var' for final keyword
   // and also we can declare or not declare data type is not compulsorily for 'final' after
   //final is initialized at the runtime.
+  //final const_name;
+  // or
+  // final data_type const_name
+
+
   final int finals = 20;
+  final rersverd=10;
   print(finals);
+  print(rersverd);
+
+
   //we cannot use 'var' for const keyword
   // and also we can declare or not declare data type is not compulsorily for 'const' after
   //The const variable will be initialized at compile-time.
   //It cannot be assigned to any function or any class constructor.
+  //const const_name;
+  //  or
+  //  const data_type const_name
   const consts = 10;
+  const int how=10;
   print(consts);
+  print(how);
+  Enumers.values.forEach((v) => print('value: $v, index: ${v.index}'));
 }
+
+
 
 String returnIt() {
   return "sai";
 }
+
